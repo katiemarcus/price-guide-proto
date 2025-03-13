b30c7d413bcda1374a5982889fff2aba3352c9e4
katie <katie@katies-MacBook-Air.local>
2025-03-13 16:22:20 +0000

    UI styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tailoring Service</h1>
+        <h1>The Seam Price Guide</h1>
       </header>
       <Chat />
     </div>
